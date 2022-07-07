4810161d64b0653b96e8773fd58552d17bd21d00
Ju Dantas <jucienyds@gmail.com>
2022-07-07 15:14:50 -0300

    feat: adiciona comentario

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import './index.css';
 
 export const App = () => {
 
+  // obtem um valor previamente que estiver salvo no banco, caso nao ele seta o valor como 10
   const initialState = localStorage.getItem("count") || 10
+  // cria variavel de estado
   let [count, setCount] = useState(initialState)
 
   return (
--- src/components/controles.js
+++ src/components/controles.js
@@ -5,17 +5,20 @@ export const Controles = ({ count, setCount }) => {
         localStorage.setItem("count", value)
     }
 
+    // funcao que salva o contador na variavel de estado e no localStorage
     const setCountAndSave = (newCount) => {
         console.log(newCount)
         setCount(newCount)
         save(newCount)
     }
 
+    // funcao para aumentar o valor
     const aumentarContador = function () {
         const newCount = parseInt(count) + 1
         setCountAndSave(newCount)
     }
 
+    // funcao para diminuir o valor
     const diminuirContador = function () {
         const newCount = parseInt(count) - 1
         setCountAndSave(newCount)
